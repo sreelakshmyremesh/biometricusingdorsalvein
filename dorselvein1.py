de1de3de832b0bc90313d6aa4e0c647240d96e15
sreelakshmyremesh <62588452+sreelakshmyremesh@users.noreply.github.com>
2021-05-12 20:35:08 +0530

    Add files via upload

---
+++ dorselvein1.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 12 19:49:04 2021
+
+@author: USER
+"""
+
+import scipy.ndimage
+import collections
+import cv2
+import pandas as pd
+import numpy as np
+import math
+import time
+from math import sqrt
+from math import sqrt
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import skimage
+import glob
+from skimage.morphology import skeletonize as skelt
+from skimage.morphology import thin
+from skimage.morphology import covex_hull_image
+from skimage.morphology import *
+import numpy as np
+import csv
+def skeletonize(image_input):
+    image=np.zeros_like(image_input)
+    image[image_input==0]=1.0
+    ouput=np.zeros_like(image_input)
+    skeletone=skelt(image)
+    output[skelton]=255
+    cv2.bitwise_not(ouput,output)
+    return output
+def thinning_morph(image, kernal):
+    """ Thinning image using morphological operatiosn
+    :param image: 2d array uint8
+    :param kernal:3x3 2d array unint8
+    :return: thin images"""
+    thinning_image=np.zeros_like(image)
+    img=image.copy()
